e946f055f0dc11e92896acba54288ad4d2517179
Adrian Scripca <benishor@gmail.com>
2014-11-25 09:14:26 +0200

    Remove bloat from Identity. Be consistent and use the constructor to set object invariants as Email does

--- src/main/java/ro/scene/hq/holidayrequester/Identity.java
+++ src/main/java/ro/scene/hq/holidayrequester/Identity.java
@@ -4,16 +4,8 @@ public class Identity {
     public String email;
     public String name;
 
-    public static Identity fromEmailAndName(String email, String name) {
-        Identity result = new Identity();
-        result.email = email;
-        result.name = name;
-        return result;
-    }
-
-    public static Identity fromEmail(String email) {
-        Identity result = new Identity();
-        result.email = email;
-        return result;
+    public Identity(String email, String name) {
+        this.email = email;
+        this.name = name;
     }
 }
--- src/main/java/ro/scene/hq/holidayrequester/Launcher.java
+++ src/main/java/ro/scene/hq/holidayrequester/Launcher.java
@@ -5,8 +5,8 @@ import java.time.LocalDate;
 public class Launcher {
     public static void main(String... args) {
         new HolidayRequest()
-                .fromEmployee(Identity.fromEmailAndName("benishor@gmail.com", "Adrian Scripca"))
-                .toManager(Identity.fromEmail("boss@iquestgroup.com"))
+                .fromEmployee(new Identity("benishor@gmail.com", "Adrian Scripca"))
+                .toManager(new Identity("boss@iquestgroup.com", "Dilbert Boss"))
                 .startingOn(LocalDate.of(2014, 11, 24))
                 .lastingForDays(5)
                 .send();
